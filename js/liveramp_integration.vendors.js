371be6ec2b161bfd8bd3f6be9c07632befe5681c
Anastazia Zeks <anastazia.zeks@agiledrop.com>
2020-07-24 13:30:44 +0200

    LiveRamp script included in head tag. Removed block and templates.

--- js/liveramp_integration.vendors.js
+++ js/liveramp_integration.vendors.js
@@ -1,30 +1,25 @@
-(function (Drupal, $, drupalSettings) {
-  $(window).ready(function () {
+(function ($) {
+  let handle = setInterval(function () {
+    if (undefined !== window.__tcfapi) {
+      setVendorList($);
+      clearInterval(handle);
+    }
+  }, 50);
 
-    let handle = setInterval(function () {
-      if (undefined !== this.gdprConfiguration) {
-        this.gdprConfiguration.noticeConfig.display = false;
-        setVendorList(this);
-        clearInterval(handle);
-      }
-    }, 50);
+  let killId = setTimeout(function () {
+    for (let i = killId; i > 0; i--) {
+      clearInterval(i);
+    }
+  }, 2000);
+})(jQuery);
 
-    let killId = setTimeout(function () {
-      for (let i = killId; i > 0; i--) {
-        clearInterval(i);
-      }
-    }, 2000);
-  });
-
-  function setVendorList(w) {
-    w.__tcfapi('getVendorList', null, (tcData, success) => {
-      let data = '<p>';
-      jQuery.each(tcData.vendors, function (index, value) {
-        data = data + '<span> ID: ' + value.id + ' - NAME: ' + value.name + '</span><br />';
-      });
-      data += '</p>';
-      jQuery('#liveramp_integration-vendors').find('.details-wrapper').append(data);
+function setVendorList($) {
+  window.__tcfapi('getVendorList', null, (tcData, success) => {
+    let data = '<p>';
+    $.each(tcData.vendors, function (index, value) {
+      data = data + '<span> ID: ' + value.id + ' - NAME: ' + value.name + '</span><br />';
     });
-  }
-
-})(Drupal, jQuery, drupalSettings);
+    data += '</p>';
+    $('#liveramp_integration-vendors').find('.details-wrapper').append(data);
+  });
+}
